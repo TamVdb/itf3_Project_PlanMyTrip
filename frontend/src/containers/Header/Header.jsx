e0bf9f15fc0f6f10967a8ff714aa54f02f38155d
Tamara <vdb.tamara@gmail.com>
2024-10-14 18:24:47 +0200

    Header & Hero Section 🌈

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -1,4 +1,4 @@
-const Header = ({ onShowModal = () => { } }) => {
+const Header = ({ onShowModal }) => {
 
    const handleLoginModal = (e) => {
       e.preventDefault();
@@ -14,14 +14,13 @@ const Header = ({ onShowModal = () => { } }) => {
       <>
          <header className="shadow-lg absolute top-0 left-0 w-full z-10">
             <div className="container py-4 flex justify-between items-center">
-               <div className="font-logo text-darkBlue text-2xl sm:text-3xl">Plan My Trip</div>
+               <div className="font-logo text-custom-blue text-2xl sm:text-3xl">Plan My Trip</div>
                <nav className="flex justify-between items-center">
                   <ul className="flex items-center gap-4">
-                     <li><a href="#" onClick={handleLoginModal} className="text-lg font-medium text-darkBlue hover:text-darkerText px-4 py-4">Log in</a></li>
-                     <li><a href="#" onClick={handleSignUpModal} className="text-lg font-medium text-darkBlue hover:text-darkerText px-4 py-4">Sign up</a></li>
+                     <li><a href="#" onClick={handleLoginModal} className="text-lg font-medium text-custom-blue hover:text-darkerText px-2 py-4">Log in</a></li>
+                     <li><a href="#" onClick={handleSignUpModal} className="text-lg font-medium text-custom-blue hover:text-darkerText px-2 py-4">Sign up</a></li>
                   </ul>
                </nav>
-
             </div>
          </header>
       </>
--- frontend/src/containers/Hero/Hero.jsx
+++ frontend/src/containers/Hero/Hero.jsx
@@ -1,8 +1,35 @@
+import Header from '../Header/Header';
+import Modal from '../Modal/Modal';
+import { useState } from 'react';
+
 const Hero = ({ handleLoginPopup }) => {
+
+   const [isModalVisible, setIsModalVisible] = useState(false); // Etat initial du modal, n'est pas visible
+   const [modalType, setModalType] = useState('login'); // 'login' ou 'signup'
+
+   const handleWhichModal = (type) => {
+      setModalType(type); // Définit le type de modal à afficher (login ou signup)
+      setIsModalVisible(true); // Affiche le modal
+   };
+
+   const handleCloseModal = () => {
+      setIsModalVisible(false); // Ferme le modal
+   };
+
+   const handleSwitchToLogin = () => {
+      setModalType('login'); // Bascule vers le formulaire Login
+   };
+
+   const handleSwitchToSignup = () => {
+      setModalType('signup'); // Bascule vers le formulaire Signup
+   };
+
    return (
       <>
+         <Header onShowModal={handleWhichModal} />
+
          <div className="container py-4 flex flex-col justify-center items-center overflow-visible">
-            <h1 className="font-title font-bold text-wine text-center text-3xl sm:text-5xl lg:text-6xl pt-36 sm:pt-44 md:w-[900px]">
+            <h1 className="font-title font-bold text-custom-wine text-center text-3xl sm:text-5xl lg:text-6xl pt-36 sm:pt-44 md:w-[900px]">
                Your activity planner for the perfect travel experience
             </h1>
             <p className="text-darkerText text-lg font-semibold md:w-[850px] text-center pt-6">
@@ -10,12 +37,18 @@ const Hero = ({ handleLoginPopup }) => {
             </p>
             <button
                onClick={handleLoginPopup}
-               className="bg-orange-500 text-white py-1 px-5 rounded-full mt-7 hover:bg-orange-500/80 transition-200 "
+               className="bg-custom-orange text-white py-2 px-5 rounded-xl mt-7 hover:bg-orange-500/80 transition-200 "
             >
-               Join Now
+               Start planning
             </button>
-
          </div>
+
+         <Modal
+            visible={isModalVisible}
+            modalType={modalType}
+            onClose={handleCloseModal}
+            switchToLogin={handleSwitchToLogin}
+            switchToSignup={handleSwitchToSignup} />
       </>
    );
 };
