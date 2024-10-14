7823563ee01c872192df61e7c892cee71d4f167c
Tamara <vdb.tamara@gmail.com>
2024-10-14 18:21:59 +0200

    Update Tailwind Config ⚙️

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,20 +8,6 @@ module.exports = {
       './src/**/*.{js,ts,jsx,tsx}',
    ],
    theme: {
-      colors: {
-         transparent: 'transparent',
-         current: 'currentColor',
-         'text': '#4c5665',
-         'darkerText': '#2f2e38',
-         'darkBlue': '#1d3557',
-         'lightBlue': '#a8dae3',
-         'custom-yellow': '#fbb13c',
-         'lightYellow': '#fdd48a',
-         'custom-orange': '#f28705',
-         'wine': '#951f52',
-         'lightWine': '#b42263',
-      },
-
       fontFamily: {
          'title': ['Josefin', 'sans-serif'],
          'logo': ['Vacaciones', 'sans-serif'],
@@ -30,6 +16,18 @@ module.exports = {
       extend: {
          fontFamily: {
             'sans': ['Quicksand', ...defaultTheme.fontFamily.sans],
+         },
+         colors: {
+            ...colors,
+            'text': '#4c5665',
+            'darkerText': '#2f2e38',
+            'custom-blue': '#1d3557',
+            'custom-lightBlue': '#a8dae3',
+            'custom-yellow': '#fbb13c',
+            'custom-lightYellow': '#fdd48a',
+            'custom-orange': '#f28705',
+            'custom-wine': '#951f52',
+            'custom-lightWine': '#b42263',
          },
          container: {
             center: true,
