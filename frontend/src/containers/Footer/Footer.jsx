aaf138dad81c23468e66e58ed1e11d24255e9342
Tamara Vdb <vdb.tamara@gmail.com>
2024-11-05 14:48:37 +0100

    Update CSS Footer 🪄

--- frontend/src/containers/Footer/Footer.jsx
+++ frontend/src/containers/Footer/Footer.jsx
@@ -4,13 +4,12 @@ const Footer = () => {
    return (
       <>
          <footer className="bg-custom-blue p-2">
-            <div className="container py-4 flex justify-between items-center">
+            <div className="container py-4 flex flex-col lg:flex-row justify-center lg:justify-between items-center">
 
-               <div className=" flex font-logo text-custom-lightBlue text-2xl sm:text-3xl">Plan My Trip</div>
+               <div className="font-logo text-custom-lightBlue text-2xl sm:text-3xl">Plan My Trip</div>
                <div className="flex flex-col">
-                  <p className="text-right text-white">Legal Notice | Privacy Policy | Cookies Policy</p>
-                  <p className="text-right inline-flex align-center text-white mt-1">© 2024 - Plan My Trip | All rights reserved •
-                     Made with <FaHeart className="text-custom-lightBlue mx-2" /> by Tamara Vandebroeck</p>
+                  <p className="text-center lg:text-right text-white pt-2 lg:pt-0 text-sm lg:text-base">Legal Notice | Privacy Policy | Cookies Policy</p>
+                  <p className="text-center lg:text-right text-white mt-1 text-sm lg:text-base">© 2024 - Plan My Trip | All rights reserved • Made with <FaHeart className="inline-block text-custom-lightBlue" /> by Tamara Vandebroeck</p>
                </div>
             </div>
          </footer>
