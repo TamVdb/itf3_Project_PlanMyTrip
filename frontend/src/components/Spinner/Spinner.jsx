29317194b00c2fe1cd638b7134abb0f0be2654fe
Tamara Vdb <vdb.tamara@gmail.com>
2024-11-18 11:31:04 +0100

    Clean some CSS 🧼

--- frontend/src/components/Spinner/Spinner.jsx
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
    return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center mx-auto">
          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-custom-blue"></div>
       </div>
    );
