a0e3d0515a0a0426a394a2bfea0445039ddd3089
Tamara Vdb <vdb.tamara@gmail.com>
2024-11-18 14:33:56 +0100

    Add scroll top modal

--- frontend/src/containers/TripModal/TripModal.jsx
+++ frontend/src/containers/TripModal/TripModal.jsx
@@ -18,7 +18,7 @@ const TripModal = () => {
 
    return (
       <div className="fixed top-0 left-0 w-full h-full z-50 bg-gray-700/50">
-         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl shadow-md w-[90%] lg:w-1/2 xl:w-1/3 2xl:w-1/4  mx-auto bg-white">
+         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl shadow-md w-[90%] lg:w-1/2 xl:w-1/3 2xl:w-1/4 mx-auto bg-white max-h-[80vh] overflow-y-auto">
             <button onClick={handleCloseModal} className="absolute top-4 right-4 font-semibold rounded-full bg-custom-blue text-white w-7 h-7">X</button>
             {/* Affiche AddTrip ou UpdateTrip en fonction de modalType */}
             {modalType === 'addTrip' && <TripAddForm />}
