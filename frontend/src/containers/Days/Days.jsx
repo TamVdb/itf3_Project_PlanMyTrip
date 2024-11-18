af724cf36b63324e630d09bf7d6cc2f27c1ce950
Tamara Vdb <vdb.tamara@gmail.com>
2024-11-18 16:12:06 +0100

    Display days on 4 columns

--- frontend/src/containers/Days/Days.jsx
+++ frontend/src/containers/Days/Days.jsx
@@ -1,7 +1,7 @@
 const Day = ({ nbDay }) => {
    return (
       <>
-         <div className="bg-white flex flex-col border border-custom-blue rounded-xl w-full lg:w-[calc(33.33%-1rem)] gap-4 justify-between">
+         <div className="bg-white flex flex-col border border-custom-blue rounded-xl w-full lg:w-[calc(33.33%-1rem)] xl:w-[calc(25%-1rem)] gap-4 justify-between">
             <div className="rounded-t-xl bg-custom-blue py-2">
                <p className="font-title text-lg text-white text-center">Day {nbDay}</p>
             </div>
