6944e1aa3941b6c73d56b25f9b0f3a4b19b7361e
Tamara Vdb <vdb.tamara@gmail.com>
2024-11-18 11:32:12 +0100

    Add redirection to trips if user state

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,10 +1,22 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Hero from '../../containers/Hero/Hero';
 import { openModal } from '../../store/modal/modal.slice';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const HomePage = () => {
 
    const dispatch = useDispatch();
+   const navigate = useNavigate();
+
+   // Select user from state
+   const { user } = useSelector((state) => state.auth);
+
+   useEffect(() => {
+      if (user) {
+         navigate('/trips');
+      }
+   }, [user, navigate]);
 
    const handleStartPlanningClick = () => {
       dispatch(openModal('login'));
