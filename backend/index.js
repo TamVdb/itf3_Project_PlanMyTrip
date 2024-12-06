d191bfc8d279ea7d4e52b74036b2e2cfb9ef00c6
Tamara Vdb <vdb.tamara@gmail.com>
2024-12-06 16:24:29 +0100

    Still trying to deploy on vercel

--- backend/index.js
+++ backend/index.js
@@ -36,3 +36,5 @@ app.use('/api/trip', activityRoutes);
 // app.listen(port, () => {
 //    console.log(`Server is running on port ${port}`);
 // });
+
+module.exports = app; // Export app for Vercel
